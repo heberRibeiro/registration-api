c25bc1a5d8482a2e063290c401f543e6362e52c8
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-07-16 14:03:11 -0300

    Login required

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 router.post('/', userController.create);
-router.get('/', userController.list);
+router.get('/', loginRequired, userController.list);
 router.get('/:id', userController.fromID);
 router.put('/:id', userController.update);
 
