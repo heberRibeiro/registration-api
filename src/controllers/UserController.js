707378270d7354e836351bb5cb056990fc6d07d1
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-07-16 11:57:12 -0300

    User crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from '../models/User';
 
 class UserController {
+  // create
   async create(req, res) {
     try {
       const newUser = await User.create(req.body);
@@ -8,6 +9,77 @@ class UserController {
       res.json(newUser);
     } catch (e) {
       res.status(400).json({ erros: e.errors.map(err => err.message) });
+    }
+  }
+
+  // list all
+  async list(req, res) {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      res.json(null);
+    }
+  }
+
+  // fromID
+  async fromID(req, res) {
+    try {
+      const { id } = req.params;
+      const users = await User.findByPk(id);
+      res.json(users);
+    } catch (error) {
+      res.json(null);
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const users = await User.findByPk(req.params.id);
+
+      if (!users) {
+        res.status(400).json({
+          erros: ['Usuário não existe'],
+        });
+      }
+
+      const newData = await users.update(req.body);
+
+      res.json(newData);
+    } catch (error) {
+      res.json(null);
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const users = await User.findByPk(req.params.id);
+
+      if (!users) {
+        res.status(400).json({
+          erros: ['Usuário não existe'],
+        });
+      }
+
+      await users.destroy();
+
+      res.json(null);
+    } catch (error) {
+      res.json(null);
     }
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,8 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.create);
+router.get('/', userController.list);
+router.get('/:id', userController.fromID);
+router.put('/:id', userController.update);
 
 export default router;
